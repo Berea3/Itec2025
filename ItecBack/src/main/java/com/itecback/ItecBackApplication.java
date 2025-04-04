6a26693b6c3e2a1de161267be98ce049dced8c06
Berendea Andrei <berendeaandrei03@gmail.com>
2025-04-04 19:39:28 +0300

    projects initiated

---
+++ ItecBack/src/main/java/com/itecback/ItecBackApplication.java
@@ -0,0 +1,13 @@
+package com.itecback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItecBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItecBackApplication.class, args);
+    }
+
+}

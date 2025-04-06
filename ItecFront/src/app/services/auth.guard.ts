929853ce776f1cf128c0f6a35b2cc30c1e08db95
Berendea Andrei <berendeaandrei03@gmail.com>
2025-04-06 08:10:19 +0300

    agusta

--- ItecFront/src/app/services/auth.guard.ts
+++ ItecFront/src/app/services/auth.guard.ts
@@ -1,9 +1,10 @@
-import { CanActivateFn } from '@angular/router';
+import {CanActivateFn, Router} from '@angular/router';
 import {SecurityService} from './security.service';
 import {inject} from '@angular/core';
 
 export const authGuard: CanActivateFn = (route, state) => {
     const securityService=inject(SecurityService);
+    const router=inject(Router);
     if (securityService.isLoggedIn()) return true;
     else router.navigateByUrl("/login");
     return false;
